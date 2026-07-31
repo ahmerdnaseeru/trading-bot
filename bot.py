b39c72a6b4fd2c7eb10497800243721a63200f2a
ahmerdnaseeru <ahmerdnaseeru@gmail.com>
2026-07-31 12:29:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,21 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+@app.route('/')
+def home(): 
+    return "Bot is alive"
+
+def run(): 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+
+Thread(target=run).start()
+
+import asyncio
+import requests
+from telethon import TelegramClient, events, Button
+from web3 import web3
 import os
 import asyncio
 import requests
